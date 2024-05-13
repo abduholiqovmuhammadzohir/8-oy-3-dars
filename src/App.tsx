8492a6cc1564415d54ea1e305912fd09d5c667d0
abduholiqovmuhammadzohir <135995518+abduholiqovmuhammadzohir@users.noreply.github.com>
2024-05-13 22:29:29 +0500

    completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import TodoList from "./components/TodoList";
+import "./CSS/App.css"
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="App">
+      <div className="header">
+        <div className="logoside">
+        <h1>What To Do</h1>
+        </div>
+     
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <TodoList />
+    </div>
+  );
 }
 
-export default App
+export default App;
